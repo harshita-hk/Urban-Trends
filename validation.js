f81ff947383212816dbc89f24f5fb3df8a734c3f
harshita-hk <57785875+harshita-hk@users.noreply.github.com>
2020-12-27 06:34:09 +0530

    Added all files

---
+++ validation.js
@@ -0,0 +1,126 @@
+function login()                                    
+ { 
+    var name = document.f1.name.value;   
+    var Password = document.f1.Password.value;
+
+  if (name == "" || Password == "")
+     {
+      alert("Please Fill out all the details");
+      return false;
+     }
+
+  else if(Password.length<8)
+     {
+      alert("Password must be at least 8 characters long.");
+      return false;
+     }    
+
+else if(Password.search(/[0-9]/)==-1)
+     {
+      alert("Password must contain 1 number.");
+      return false;
+     }    
+
+else if(Password.search(/[a-z]/)==-1)
+     {
+      alert("Password must contain 1 lowercase.");
+      return false;
+     }    
+
+else if(Password.search(/[A-Z]/)==-1)
+     {
+      alert("Password must contain 1 uppercase.");
+      return false;
+     }    
+
+else if(Password.search(/[/(!\@\#\$\%\^\&\*\:\"\<\>\?]/)==-1)
+     {
+      alert("Password must contain 1 special chr.");
+      return false;
+     }   
+
+ }
+
+function signup()
+{
+    var name1 = document.f2.fname.value;
+    var mail = document.f2.mail.value;
+    var no = document.f2.pno.value;
+    var pass = document.f2.pass.value;
+    var cpass = document.f2.cpass.value;
+
+  if(pass=="" || cpass=="" || name1==""  || mail=="" || no=="")
+  {
+    alert("Please Fill out all the details");
+    return false;
+  }
+
+  else if (no>10 && no<10) 
+  {
+    alert("Please Fill out the correct Phone number");
+  }
+
+ else if(pass.length<8)
+     {
+      alert("Password must be at least 8 characters long.");
+      return false;
+     }    
+
+else if(pass.search(/[0-9]/)==-1)
+     {
+      alert("Password must contain 1 number.");
+      return false;
+     }    
+
+else if(pass.search(/[a-z]/)==-1)
+     {
+      alert("Password must contain 1 lowercase.");
+      return false;
+     }    
+
+else if(pass.search(/[A-Z]/)==-1)
+     {
+      alert("Password must contain 1 uppercase.");
+      return false;
+     }    
+
+else if(pass.search(/[/(!\@\#\$\%\^\&\*\:\"\<\>\?]/)==-1)
+     {
+      alert("Password must contain 1 special chr.");
+      return false;
+     } 
+
+ else if(cpass!=pass)
+  {  
+   alert("Both the Passwords must be same");
+   return false;   
+  }
+}
+
+function pas()
+ {
+    var no = document.f3.mobileno.value;
+    var otp = document.f3.otp.value;
+
+  if(no=="")
+  {
+    alert("Plz enter the Mobile Number.");
+    return false;
+  }
+  else if (no>10 && no<10) 
+  {
+    alert("Please Fill out the correct Phone number");
+  }  
+  else if (otp>7 && no<7) 
+  {
+    alert("Please Fill out the correct OTP");
+  }   
+  else if(isNaN(no))  {
+     alert("Please Fill out the correct Phone number");
+  }
+
+else if(isNaN(otp)) {
+     alert("Please Fill out the correct Phone number");
+  }
+
+}
